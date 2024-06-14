504348bc273a57e8b3760070d6f5e3e39499948d
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-06-14 20:29:06 -0300

    Set up done

--- android/app/src/main/java/com/movie_ticket_app/MainActivity.kt
+++ android/app/src/main/java/com/movie_ticket_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.movie_ticket_app
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -11,6 +12,11 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+   
   override fun getMainComponentName(): String = "movie_ticket_app"
 
   /**
