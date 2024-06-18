573eb81fefe50595539509783e391c72a9ea9213
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-06-18 17:34:15 -0300

    HomeScreen | Loading component and TextInput added

---
+++ src/components/InputHeader.tsx
@@ -0,0 +1,63 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcon from './CustomIcon';
+
+const InputHeader = () => {
+  const [searchText, setSearchText] = useState<string>('');
+
+  return (
+    <View style={styles.inputBox}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={textInput => setSearchText(textInput)}
+        placeholder="Search a Movie..."
+        placeholderTextColor={COLORS.WhiteRGBA32}
+      />
+      <TouchableOpacity style={styles.searchIcon}>
+        <CustomIcon
+          name="search"
+          color={COLORS.Orange}
+          size={FONTSIZE.size_20}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputBox: {
+    display: 'flex',
+    paddingVertical: SPACING.space_8,
+    paddingHorizontal: SPACING.space_24,
+    borderWidth: 2,
+    borderColor: COLORS.WhiteRGBA15,
+    borderRadius: BORDERRADIUS.radius_25,
+    flexDirection: 'row',
+  },
+  textInput: {
+    width: '90%',
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  searchIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: SPACING.space_10,
+  },
+});
+
+export default InputHeader;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,74 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import {COLORS, SPACING} from '../theme/theme';
+import InputHeader from '../components/InputHeader';
+
+const {width, height} = Dimensions.get('window');
 
 const HomeScreen = () => {
+  const [nowPlayingMoviesList, setNowPlayingMoviesList] =
+    useState<any>(undefined);
+  const [popularMoviesList, setPopularMoviesList] = useState<any>(undefined);
+  const [upcomingMoviesList, setUpcomingMoviesList] = useState<any>(undefined);
+
+  if (
+    nowPlayingMoviesList == undefined &&
+    nowPlayingMoviesList == null &&
+    popularMoviesList == undefined &&
+    popularMoviesList == null &&
+    upcomingMoviesList == undefined &&
+    upcomingMoviesList == null
+  ) {
+    return (
+      <ScrollView
+        style={styles.container}
+        bounces={false}
+        contentContainerStyle={styles.scrollViewContainer}>
+        <StatusBar hidden />
+
+        <View style={styles.inputHeaderContainer}>
+          <InputHeader />
+        </View>
+
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size={'large'} color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+    );
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>HomeScreen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: COLORS.Black,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  inputHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
+  },
+});
 
 export default HomeScreen;
