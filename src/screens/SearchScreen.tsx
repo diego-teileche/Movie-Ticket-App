ea3b673fdc6187b00142305a2112abc95c86ca9f
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-02 19:58:02 -0300

    SearchScreen | Input and images added, screen finished

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,14 +1,84 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Dimensions,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import {COLORS, SPACING} from '../theme/theme';
+import {baseImagePath, searchMovies} from '../api/apicalls';
+import InputHeader from '../components/InputHeader';
+import SubMovieCard from '../components/SubMovieCard';
 
-const SearchScreen = () => {
+const {width, height} = Dimensions.get('screen');
+
+const SearchScreen = ({navigation}: any) => {
+  const [searchList, setSearchList] = useState([]);
+
+  const searchMoviesFunction = async (name: string) => {
+    try {
+      let response = await fetch(searchMovies(name));
+      let json = await response.json();
+
+      setSearchList(json.results);
+    } catch (error) {
+      console.log('Something went wrong in searchMoviesList: ', error);
+    }
+  };
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={styles.container}>
+      <StatusBar hidden />
+
+      <View>
+        <FlatList
+          data={searchList}
+          keyExtractor={(item: any) => item.id}
+          bounces={false}
+          numColumns={2}
+          ListHeaderComponent={
+            <View style={styles.inputHeaderContainer}>
+              <InputHeader searchFunction={searchMoviesFunction} />
+            </View>
+          }
+          contentContainerStyle={styles.centerContainer}
+          renderItem={({item, index}) => (
+            <SubMovieCard
+              shouldMarginatedAtEnd={false}
+              shouldMarginatedAround={true}
+              cardFunction={() =>
+                navigation.push('MovieDetails', {movieid: item.id})
+              }
+              cardWidth={width / 2 - SPACING.space_12 * 2}
+              title={item.original_title}
+              imagePath={baseImagePath('w342', item.poster_path)}
+            />
+          )}
+        />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    width,
+    alignItems: 'center',
+    backgroundColor: COLORS.Black,
+  },
+  inputHeaderContainer: {
+    display: 'flex',
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
+    marginBottom: SPACING.space_28 - SPACING.space_12,
+  },
+  centerContainer: {
+    alignItems: 'center',
+  },
+});
 
 export default SearchScreen;
