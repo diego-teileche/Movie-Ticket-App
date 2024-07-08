32489e6dea63d2f0dda2cd2b12f583793841416a
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-08 17:00:23 -0300

    TicketScreen | Styles added

--- src/screens/TicketScreen.tsx
+++ src/screens/TicketScreen.tsx
@@ -1,6 +1,7 @@
 import {
   Image,
   ImageBackground,
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -9,7 +10,13 @@ import {
 import React, {useEffect, useState} from 'react';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import AppHeader from '../components/AppHeader';
-import {BORDERRADIUS, COLORS, SPACING} from '../theme/theme';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
 import LinearGradient from 'react-native-linear-gradient';
 import CustomIcon from '../components/CustomIcon';
 
@@ -46,7 +53,7 @@ const TicketScreen = ({navigation, route}: any) => {
   }
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <StatusBar hidden />
 
       <View style={styles.appHeaderContainer}>
@@ -63,20 +70,48 @@ const TicketScreen = ({navigation, route}: any) => {
           style={styles.ticketBGImage}>
           <LinearGradient
             colors={[COLORS.OrangeRGBA0, COLORS.Orange]}
-            style={styles.linearGradient}></LinearGradient>
+            style={styles.linearGradient}>
+            <View
+              style={[
+                styles.blackCircle,
+                {position: 'absolute', bottom: -40, left: -40},
+              ]}
+            />
+
+            <View
+              style={[
+                styles.blackCircle,
+                {position: 'absolute', bottom: -40, right: -40},
+              ]}
+            />
+          </LinearGradient>
         </ImageBackground>
 
         <View style={styles.linear}></View>
 
         <View style={styles.ticketFooter}>
+          <View
+            style={[
+              styles.blackCircle,
+              {position: 'absolute', top: -40, left: -40},
+            ]}
+          />
+
+          <View
+            style={[
+              styles.blackCircle,
+              {position: 'absolute', top: -40, right: -40},
+            ]}
+          />
+
           <View style={styles.ticketDateContainer}>
-            <View>
+            <View style={styles.subtitleContainer}>
               <Text style={styles.dateTitle}>{ticketData?.date.date}</Text>
 
               <Text style={styles.dateSubtitle}>{ticketData?.date.day}</Text>
             </View>
 
-            <View>
+            <View style={styles.subtitleContainer}>
               <CustomIcon name="clock" style={styles.clockIcon} />
 
               <Text style={styles.dateSubtitle}>{ticketData?.time}</Text>
@@ -115,7 +150,7 @@ const TicketScreen = ({navigation, route}: any) => {
           />
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -128,7 +163,7 @@ const styles = StyleSheet.create({
   appHeaderContainer: {
     marginHorizontal: SPACING.space_36,
     marginTop: SPACING.space_20 * 2,
-    marginBottom: SPACING.space_36 * 4,
+    marginBottom: SPACING.space_36,
   },
   ticketContainer: {
     flex: 1,
@@ -146,16 +181,71 @@ const styles = StyleSheet.create({
   linearGradient: {
     height: '70%',
   },
-  linear: {},
-  ticketFooter: {},
-  ticketDateContainer: {},
-  dateTitle: {},
-  dateSubtitle: {},
-  clockIcon: {},
-  ticketSeatContainer: {},
-  subtitleContainer: {},
-  subHeading: {},
-  barCodeImage: {},
+  linear: {
+    borderTopColor: COLORS.Black,
+    borderTopWidth: 3,
+    width: 300,
+    alignSelf: 'center',
+    backgroundColor: COLORS.Orange,
+    borderStyle: 'dashed',
+  },
+  ticketFooter: {
+    backgroundColor: COLORS.Orange,
+    width: 300,
+    alignItems: 'center',
+    paddingBottom: SPACING.space_36,
+    alignSelf: 'center',
+    borderBottomLeftRadius: BORDERRADIUS.radius_25,
+    borderBottomRightRadius: BORDERRADIUS.radius_25,
+  },
+  ticketDateContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: SPACING.space_10,
+  },
+  dateTitle: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+  },
+  dateSubtitle: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  clockIcon: {
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+    marginVertical: SPACING.space_8,
+  },
+  ticketSeatContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: SPACING.space_10,
+  },
+  subtitleContainer: {
+    alignItems: 'center',
+  },
+  subHeading: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.White,
+  },
+  barCodeImage: {
+    marginVertical: SPACING.space_10,
+    height: 50,
+    aspectRatio: 158 / 52,
+  },
+  blackCircle: {
+    height: 80,
+    width: 80,
+    borderRadius: 80,
+    backgroundColor: COLORS.Black,
+  },
 });
 
 export default TicketScreen;
