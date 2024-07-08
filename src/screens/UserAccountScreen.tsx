b71a22cc4e7575aa8dc77ea456112f3e3a7c0e47
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-08 18:03:22 -0300

    UserAccountScreen | Header added

--- src/screens/UserAccountScreen.tsx
+++ src/screens/UserAccountScreen.tsx
@@ -1,14 +1,35 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {COLORS, SPACING} from '../theme/theme';
+import AppHeader from '../components/AppHeader';
 
-const UserAccountScreen = () => {
+const UserAccountScreen = ({navigation}: any) => {
   return (
-    <View>
-      <Text>UserAccountScreen</Text>
+    <View style={styles.container}>
+      <StatusBar hidden />
+
+      <View style={styles.appHeaderContainer}>
+        <AppHeader
+          name="close"
+          header="My Profile"
+          action={() => navigation.goBack()}
+        />
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  appHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_20 * 2,
+    marginBottom: SPACING.space_36,
+  },
+});
 
 export default UserAccountScreen;
